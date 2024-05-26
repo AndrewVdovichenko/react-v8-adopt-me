65f142edc3301657c02d6acb12ea7460d31cb1eb
Andrew Vdovichenko <AndrewVdovichenko@users.noreply.github.com>
2024-05-26 12:00:07 +0500

    03 core react (#2)
    * convert to jsx component
    
    * move vite config file
    
    * add SearchParams
    
    * fetch pets using useEffect
    
    * add custom hook useBreedList
    
    * handle form submitting
    
    * make component composition

---
+++ src/SearchParams.jsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import Results from "./Results";
+import useBreedList from "./useBreedList";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [pets, setPets] = useState([]);
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = useBreedList(animal);
+
+  const handleAnimalChange = (e) => {
+    setAnimal(e.target.value);
+    setBreed("");
+  };
+  const handleBreedChange = (e) => {
+    setBreed(e.target.value);
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    requestPets();
+  };
+
+  const requestPets = async () => {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`,
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  };
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div className="search-params">
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={handleAnimalChange}
+            onBlur={handleAnimalChange}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={handleBreedChange}
+            onBlur={handleBreedChange}
+          >
+            <option />
+            <option>
+              {breeds.map((breed) => (
+                <option key={breed} value={breed}>
+                  {breed}
+                </option>
+              ))}
+            </option>
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
